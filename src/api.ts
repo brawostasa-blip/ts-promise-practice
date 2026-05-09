7012d6a66c09c97d9cdfc5c120d170ec73909006
brawostasa-blip <brawostasa@gmail.com>
2026-05-09 19:40:41 +0500

    Update api.ts
    Исправлена надежность. Добавлена обработок ошибок при запросе

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,8 @@
 export async function loadPosts() {
   const response = await fetch("https://json-placeholder.mock.beeceptor.com/posts")
+  if (!response.ok) {
+    throw new Error(Ошибка загрузки постов: ${response.status} ${response.statusText})
+  }
   const data = await response.json()
   return data
 }
